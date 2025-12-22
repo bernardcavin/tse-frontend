7c7f3858bde84c7253881b7f2aef4330f1d22703
Bernard Cavin Ronlei Sihombing <bernardcavin@outlook.com>
2025-12-22 15:29:34 +0700

    fix: changes

--- dist/assets/page-7q-nXRSK.js
+++
@@ -1,1 +0,0 @@
-import{r,cm as s,j as e,cn as x,b as p,B as i}from"./index-CaG8M5h2.js";const d=r.forwardRef(({children:t,title:a="",p:o="md",meta:n,withFooter:j=!0,...c},m)=>(r.useEffect(()=>(s.complete(),()=>s.start()),[]),e.jsxs(e.Fragment,{children:[e.jsxs(x,{children:[e.jsx("title",{children:`${a} | ${p.name}`}),n]}),e.jsx(i,{ref:m,...c,p:o,children:t})]})));export{d as P};
---
+++ dist/assets/text-input-C7Ly9XXk.js
@@ -0,0 +1,1 @@
+import{r as p,j as a,ad as n,ac as u}from"./index-hFCbRvL1.js";import{u as e}from"./form-tngHacic.js";const c=p.forwardRef(({name:r,...t},o)=>{const s=e();return a.jsx(n,{ref:o,...t,...s.getInputProps(r)},s.key(r))}),m=p.forwardRef(({name:r,...t},o)=>{const s=e();return a.jsx(u,{ref:o,...t,...s.getInputProps(r)},s.key(r))});export{c as P,m as T};
